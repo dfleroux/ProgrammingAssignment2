472e3acd8571a6c9c219789a1d131e5a721beacb
dfleroux <dfleroux@users.noreply.github.com>
2015-02-19 21:37:11 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,5 @@
+##test2 on home computer (proxy issue)
+
 ##test change dfleroux/first_time_user, attemtping first commit submission?
 ## Put comments here that give an overall description of what your
 ## functions do
