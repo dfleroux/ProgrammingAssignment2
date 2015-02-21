ee60c684092325de02d5244e37a7bb493010e264
dfleroux <dfleroux@users.noreply.github.com>
2015-02-21 20:25:34 +0100

    Update cachematrix.R
    version Denis 2 of assignment

--- cachematrix.R
+++ cachematrix.R
@@ -3,32 +3,42 @@
 ##in order to avoid repeating the same variable calculation thus saving computig time
 
 ##1.makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
-## <<- operator which can be used to assign a value to an object in an environment that 
-##is different from the current environment
 
-makeCacheMatrix <- function(u = matrix()) {
-  s<-NULL
+
+##original matrix is "M"; inverted matrix is "IM"
+
+makeCacheMatrix <- function(M = matrix()) {
+  IM<-NULL
+  
+## "<<-" operator assigns a value to the free variable "v" to the "cachesolve" function instead of the GLOBAL environment
   set<-function(v){
-    u<<-v
-    s<<-NULL
+    M<<-v
+    IM<<-NULL
   }
-  get<-function() u
-  setmatrix<-function(solve) s<<- solve
-  getmatrix<-function() s
+  get<-function() M
+  setmatrix<-function(solve) IM<<- solve
+  getmatrix<-function() IM
   list(set=set, get=get,
-       setmatrix=setmatrix,
-       getmatrix=getmatrix)
+  setmatrix=setmatrix,
+  getmatrix=getmatrix)
 }
 
-##2.cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
-cacheSolve <- function(x=matrix(), ...) {
-  s<-u$getmatrix()
-  if(!is.null(s)){
-    message("getting cached data")
-    return(s)
+##2.cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix
+##above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve
+##should retrieve the inverse from the cache.
+
+cacheSolve <- function(M=matrix(), ...) {
+  IM<-M$getmatrix()
+  ## the following "if loop" checks if the u-matrix-inverse had been
+  ##already calculated (i.e. IM is not NULL), if yes it recalls it (IM) from memory to avoid the time consuming calculation
+  if(!is.null(IM)){
+    message("gettings cached data")
+    return(IM)
   }
-  matrix<-u$get()
-  s<-solve(matrix, ...)
-  u$setmatrix(s)
-  s
+  matrix<-M$get()
+  
+  ##solve() is a R function that returns the matrix inverse
+  IM<-solve(matrix, ...)
+  M$setmatrix(IM)
+  IM
 }
